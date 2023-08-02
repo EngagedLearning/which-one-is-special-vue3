68e58cb11d24d61b47977596d64cc97fb72ed4cf
Anthony Bladek <bladekaj@comcast.net>
2023-08-02 15:55:47 -0700

    Initial commit for vue3 version. complete experiment

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import App from './App.vue';
+import router from './components/router';
+import DemographicsQuestionaire from './components/demographics/DemographicsQuestionaire.vue'; 
+import WhichOneIsSpecialPrep from './components/which-one-is-special/WhichOneIsSpecialPrep.vue';
+import SurveyQuestions from './components/survey/SurveyQuestions.vue'
 
-createApp(App).mount('#app')
+
+//import 'bootstrap/dist/css/bootstrap.min.css';
+
+const app = createApp(App);
+app.use(ElementPlus)
+app.use(router);
+app.component('WhichOneIsSpeicalPrep', WhichOneIsSpecialPrep)
+    .component('DemographicsQuestionaire', DemographicsQuestionaire)
+    .component('SurveyQuestions',SurveyQuestions);
+app.mount('#app');
+
+ 
